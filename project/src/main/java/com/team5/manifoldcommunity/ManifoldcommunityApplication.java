f1895f225f127c9ae1269f218aab406a49ae1912
Grady <1016627132@qq.com>
2019-07-24 13:29:21 +0800

    7.24

---
+++ project/src/main/java/com/team5/manifoldcommunity/ManifoldcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.team5.manifoldcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManifoldcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManifoldcommunityApplication.class, args);
+	}
+
+}
